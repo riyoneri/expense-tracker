7ba098d8fb00016ed2b53b4ed56a91b1a497a350
riyoneri <lionkaneza@gmail.com>
2022-09-28 08:39:27 +0200

    First Commit

---
+++ src/components/newExpense/newExpense.js
@@ -0,0 +1,11 @@
+import ExpenseForm from './expenseForm'
+
+import './newExpense.css'
+
+const NewExpense = () => {
+    return <div className='new-expense'>
+        <ExpenseForm />
+    </div>
+}
+
+export default NewExpense
